ca1fb95b89c6d6f859556831278ae009e15cabfb
Oksana Ilyakova <93376743+oksana0202@users.noreply.github.com>
2021-12-24 08:31:44 +0300

    Create HW3.java

---
+++ HW3.java
@@ -0,0 +1,67 @@
+/**
+ * JAVA 2 Homework3
+ *
+ * @autor Oksana Ilyakova
+ * @version 24.12.2021
+ */
+public class Homework3 {
+    public static void main(String[] args) {
+        Integer arr1[] = {1, 2, 3, 4, 5, 6, 7};
+        String arr2[] = {"A", "B", "C"} ;
+        swap(arr1,1,4);
+        swap(arr2,0,2);
+    }
+
+    public abstract class Fruit {
+        private String name="Fruit";
+        private static final float WEIGHT=0f;
+    }
+
+    public class Orange extends Fruit {
+        private String name="Orange";
+        public static final float WEIGHT=1.5f;
+    }
+
+    public class Apple extends Fruit {
+        private String name="Apple";
+        public static final float WEIGHT=1.0f;
+    }
+
+    public class  Box <T extends Fruit> {
+        private List<T> content = new ArrayList<>();
+        public void put(T fruit){
+            content.add(fruit);
+        }
+        Box(){
+            if(T instanceof Apple)
+        }
+        public void put(List<T> fruits){
+            content.addAll(fruits);
+
+        }
+        public double getWeight(){
+            if(content.size()==0) return 0;
+            return content.size()*T.WEIGHT;
+        }
+
+        public List<T> getContent() {
+            return content;
+        }
+
+        public void setContent(List<T> content) {
+            this.content = content;
+        }
+
+        @java.lang.Override
+        public java.lang.String toString() {
+            return "Box{" +
+                    "content=" + content +
+                    '}';
+        }
+        public static void main (String[] args){
+            Box box1 = new Box();
+            Box box2 = new Box();
+            System.out.println(box1.compareTo(box2));
+        }
+    }
+}
